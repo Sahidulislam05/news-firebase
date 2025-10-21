0e291345773b82c5e3efe61e4b9e33ccb7d74719
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-10-21 10:48:56 +0600

    Added Provider

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,21 +1,12 @@
 import { initializeApp } from "firebase/app";
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey,
-//   authDomain: import.meta.env.VITE_authDomain,
-//   projectId: import.meta.env.VITE_authDomain,
-//   storageBucket: import.meta.env.VITE_storageBucket,
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//   appId: import.meta.env.VITE_appId,
-// };
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDh5TJ-6JZUuY2t6WNZfh6yZlcAquEuLd4",
-  authDomain: "dragon-news-app-ead16.firebaseapp.com",
-  projectId: "dragon-news-app-ead16",
-  storageBucket: "dragon-news-app-ead16.firebasestorage.app",
-  messagingSenderId: "1009259245109",
-  appId: "1:1009259245109:web:719440e8bfce8e63200b7c",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
